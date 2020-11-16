1ecde4c9d9bc1d167c6006c19c5abcc7e9639fab
gregdu91 <55101978+gregdu91@users.noreply.github.com>
2020-11-16 21:28:41 +0100

    1

---
+++ index.js
@@ -0,0 +1,60 @@
+//3) and 4)
+const Planets = [
+     'Earth', 'Mars', 'Jupiter', 'Pluton', 'Moon'
+ ];
+ console.log(Planets);
+ Planets.forEach(element => console.log(element));
+
+
+//5)
+a + b = 5Hello
+
+b + c = Hello World!
+
+a + d = 15
+
+//6)
+a == c = true
+
+a === c = false
+
+a == x = true
+
+a != b = true
+
+a > b = false 
+
+a < b = true
+
+a >= b = false
+
+a <= b = true
+
+a >= c = true
+
+a <= c = true 
+
+
+
+//7) and 8)
+var hello = function() {
+    return 'Hello World!'
+}
+console.log(hello());
+
+//9)
+var x = 'Hi!'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
